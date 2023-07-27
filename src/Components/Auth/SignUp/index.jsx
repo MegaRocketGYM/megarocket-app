8bb55f8c173f956ff365eebf7b75fab2a9e02025
Lorenzo Mauro <lorenzomauro@MacBook-Air-de-Lorenzo.local>
2023-07-27 19:35:05 -0300

    MR-3: add logo and title in tablet and mobile view

--- src/Components/Auth/SignUp/index.jsx
+++ src/Components/Auth/SignUp/index.jsx
@@ -74,6 +74,15 @@ const SignUp = () => {
           <div className={styles.containerForm}></div>
 
           <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.signUpTitle}>
+              <img
+                onClick={() => history.push('/landing')}
+                className={styles.logo}
+                src="/assets/images/logos/logo-black.png"
+                alt="MegaRocket Logo"
+              />
+              <h2>Sign Up</h2>
+            </div>
             <div className={styles.formMembers}>
               <div className={styles.formColumn}>
                 <Input
